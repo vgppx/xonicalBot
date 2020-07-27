1d78855e86272cb1be47b231e25877f25f879c23
vgppx <41488614+vgppx@users.noreply.github.com>
2020-07-27 18:44:16 +0100

    updated speech marks to backticks

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ We offer coaching to all players no matter where you are on the ladder, learn mo
   // if the user is already registered then tell them this.
   // if the user is not registered then send them a link to register and information about registering.
   if (command === "register") {
-    message.reply("Register here https://xonical.co.uk/register for rewards and exclusive events.");
+    message.reply(`Register here https://xonical.co.uk/register for rewards and exclusive events.`);
     // if (message.author === registered) {
     //   message.author.send(`You are already registered with Xonical Networks, your profile link is` + profileURL + `. If you need to reset your password or login visit https://xonical.co.uk/login`);
     // }
@@ -76,7 +76,7 @@ We offer coaching to all players no matter where you are on the ladder, learn mo
   // COMMAND : !points will send the user a message in the channel they sent the command in, the message will display how many points they have if they are registered.
   // if the user is not registered with Xonical Networks then the bot will send them a direct message to convert them.
   if (command === "points") {
-    message.reply("!points is under development. Please register to use this command in the future - https://xonical.co.uk/register");
+    message.reply(`!points is under development. Please register to use this command in the future - https://xonical.co.uk/register`);
     // if (message.author.hasSentMessageInLast10Seconds === true) {
     //   return null;
     // }
@@ -92,42 +92,42 @@ We offer coaching to all players no matter where you are on the ladder, learn mo
     // }
   }
 
-  if (command === "kick") {
-    if (!message.member.roles.some(r => ["Administrator", "Moderator"].includes(r.name)))
-      return message.reply("Sorry, you don't have permissions to use this!");
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if (!member)
-      return message.reply("Please mention a valid member of this server");
-    if (!member.kickable)
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    let reason = args.slice(1).join(' ');
-    if (!reason) reason = "No reason provided";
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-  }
+  //   if (command === "kick") {
+  //     if (!message.member.roles.some(r => ["Administrator", "Moderator"].includes(r.name)))
+  //       return message.reply("Sorry, you don't have permissions to use this!");
+  //     let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+  //     if (!member)
+  //       return message.reply("Please mention a valid member of this server");
+  //     if (!member.kickable)
+  //       return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+  //     let reason = args.slice(1).join(' ');
+  //     if (!reason) reason = "No reason provided";
+  //     await member.kick(reason)
+  //       .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+  //     message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+  //   }
 
-  if (command === "ban") {
-    if (!message.member.roles.some(r => ["Administrator"].includes(r.name)))
-      return message.reply("Sorry, you don't have permissions to use this!");
-    let member = message.mentions.members.first();
-    if (!member)
-      return message.reply("Please mention a valid member of this server");
-    if (!member.bannable)
-      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
-    let reason = args.slice(1).join(' ');
-    if (!reason) reason = "No reason provided";
-    await member.ban(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-  }
+  //   if (command === "ban") {
+  //     if (!message.member.roles.some(r => ["Administrator"].includes(r.name)))
+  //       return message.reply("Sorry, you don't have permissions to use this!");
+  //     let member = message.mentions.members.first();
+  //     if (!member)
+  //       return message.reply("Please mention a valid member of this server");
+  //     if (!member.bannable)
+  //       return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+  //     let reason = args.slice(1).join(' ');
+  //     if (!reason) reason = "No reason provided";
+  //     await member.ban(reason)
+  //       .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+  //     message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+  //   }
 
-  if (command === "purge") {
-    const deleteCount = parseInt(args[0], 10);
-    if (!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    const fetched = await message.channel.fetchMessages({ limit: deleteCount });
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-  }
+  //   if (command === "purge") {
+  //     const deleteCount = parseInt(args[0], 10);
+  //     if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+  //       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+  //     const fetched = await message.channel.fetchMessages({ limit: deleteCount });
+  //     message.channel.bulkDelete(fetched)
+  //       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  //   }
 });
